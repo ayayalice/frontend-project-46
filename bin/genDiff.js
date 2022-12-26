a88d3e93aea8aaa4f0a20bd30e361c52a55dbaf4
Alice <aliceicelie@gmail.com>
2022-12-26 16:23:20 +0300

    created project and genDiff file

---
+++ bin/genDiff.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+import { program } from 'commander';
+
+program
+    .description('Compares two configuration files and shows a difference.')
+    .option('-V, --version', 'output the version number');
+program.parse();
+console.log(program);
