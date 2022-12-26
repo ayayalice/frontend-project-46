ba6576cbb111a5c065cfe703ed381adba36ed336
Alice <aliceicelie@gmail.com>
2022-12-26 16:33:53 +0300

    modified genDiff

--- bin/genDiff.js
+++ bin/genDiff.js
@@ -3,6 +3,8 @@ import { program } from 'commander';
 
 program
     .description('Compares two configuration files and shows a difference.')
-    .option('-V, --version', 'output the version number');
+    .option('-V, --version', 'output the version number')
+    .option( '-f, --format', '<type>  output format')
+    .arguments('<filepath1> <filepath2>')
 program.parse();
 console.log(program);
