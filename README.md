fb9f8e5461b19337ba413cb40be2c6e57ecb561d
Alice <aliceicelie@gmail.com>
2023-01-09 16:19:15 +0300

    made genDiff func

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/ayayalice/frontend-project-46/workflows/hexlet-check/badge.svg)](https://github.com/ayayalice/frontend-project-46/actions)
+[![Maintainability](https://api.codeclimate.com/v1/badges/884f1efb604de569c125/maintainability)](https://codeclimate.com/github/ayayalice/frontend-project-46/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/884f1efb604de569c125/test_coverage)](https://codeclimate.com/github/ayayalice/frontend-project-46/test_coverage)
+[![asciicast]](https://asciinema.org/a/550300)
--- bin/genDiff.js
+++ bin/genDiff.js
@@ -1,10 +1,70 @@
 #!/usr/bin/env node
-import { program } from 'commander';
+import { Command } from 'commander';
+import _ from 'lodash';
+import fs from 'fs';
+import path from 'path';
 
+const mknode = (key, value, type, meta = {}) => ({
+  key,
+  value,
+  type,
+  meta,
+});
+
+const buildAST = (objects) => {
+  const [obj1, obj2] = objects;
+  const keys = _.union(_.keys(obj1), _.keys(obj2));
+  const sortedKeys = _.sortBy(keys);
+  const nodes = sortedKeys.map((key) => {
+    const [value1, value2] = [obj1[key], obj2[key]];
+    if (!_.has(obj1, key)) return mknode(key, value2, 'added');
+    if (!_.has(obj2, key)) return mknode(key, value1, 'removed');
+    if (value1 !== value2) return mknode(key, value2, 'updated', { oldValue: value1 });
+    return mknode(key, value1, 'unchanged');
+  });
+  return nodes;
+};
+
+const symbols = {
+  removed: '-',
+  added: '+',
+  unchanged: ' ',
+};
+
+const render = (ast) => {
+  const gendiff = ast.map((el) => {
+    const {
+      type, key, value, meta,
+    } = el;
+
+    if (el.type === 'updated') {
+      return `  - ${key}: ${meta.oldValue}\n  + ${key}: ${value}`;
+    }
+    return `  ${symbols[type]} ${key}: ${value}`;
+  });
+  return `{\n${gendiff.join('\n')}\n}`;
+};
+
+const getFixturesPath = (filename) => path.resolve('__fixtures__', filename);
+
+const gendiff = (filepath1, filepath2) => {
+  const paths = [filepath1, filepath2];
+  const data = paths.map((filepath) => {
+    const fullPaht = getFixturesPath(filepath);
+    const content = fs.readFileSync(fullPaht, 'utf-8');
+    return JSON.parse(content);
+  });
+  const tree = buildAST(data);
+  return render(tree);
+};
+
+const program = new Command();
 program
-    .description('Compares two configuration files and shows a difference.')
-    .option('-V, --version', 'output the version number')
-    .option( '-f, --format', '<type>  output format')
-    .arguments('<filepath1> <filepath2>')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-V, --version', 'output the version number')
+  .option('-f, --format', '<type>  output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(gendiff(filepath1, filepath2));
+  });
 program.parse();
-console.log(program);
