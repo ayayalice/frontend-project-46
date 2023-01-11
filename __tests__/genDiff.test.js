de14c0b11d0f93430ced3ca5946b624ae4cba0e0
Alice <aliceicelie@gmail.com>
2023-01-12 01:45:05 +0300

    fix linter

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,9 +1,9 @@
-import { getFullPath, gendiff } from '../src/index.js';
+import { expect, test } from '@jest/globals';
 import fs from 'fs';
-import {expect, test} from '@jest/globals';
+import { getFullPath, gendiff } from '../src/index.js';
 
 const result = fs.readFileSync(getFullPath('result.txt'), 'utf8');
 
-test ('genDiff', () => {
-    expect(gendiff('file1.json', 'file2.json')).toBe(result)
+test('genDiff', () => {
+  expect(gendiff('file1.json', 'file2.json')).toBe(result);
 });
--- src/index.js
+++ src/index.js
@@ -1,62 +1,60 @@
 import fs from 'fs';
-import path from 'path';
-import { dirname } from 'path';
+import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import _ from 'lodash';
 
 const mknode = (key, value, type, meta = {}) => ({
-    key,
-    value,
-    type,
-    meta,
+  key,
+  value,
+  type,
+  meta,
+});
+
+const buildAST = (objects) => {
+  const [obj1, obj2] = objects;
+  const keys = _.union(_.keys(obj1), _.keys(obj2));
+  const sortedKeys = _.sortBy(keys);
+  const nodes = sortedKeys.map((key) => {
+    const [value1, value2] = [obj1[key], obj2[key]];
+    if (!_.has(obj1, key)) return mknode(key, value2, 'added');
+    if (!_.has(obj2, key)) return mknode(key, value1, 'removed');
+    if (value1 !== value2) return mknode(key, value2, 'updated', { oldValue: value1 });
+    return mknode(key, value1, 'unchanged');
   });
+  return nodes;
+};
 
-  const buildAST = (objects) => {
-    const [obj1, obj2] = objects;
-    const keys = _.union(_.keys(obj1), _.keys(obj2));
-    const sortedKeys = _.sortBy(keys);
-    const nodes = sortedKeys.map((key) => {
-      const [value1, value2] = [obj1[key], obj2[key]];
-      if (!_.has(obj1, key)) return mknode(key, value2, 'added');
-      if (!_.has(obj2, key)) return mknode(key, value1, 'removed');
-      if (value1 !== value2) return mknode(key, value2, 'updated', { oldValue: value1 });
-      return mknode(key, value1, 'unchanged');
-    });
-    return nodes;
-  };
+const symbols = {
+  removed: '-',
+  added: '+',
+  unchanged: ' ',
+};
 
-  const symbols = {
-    removed: '-',
-    added: '+',
-    unchanged: ' ',
-  };
+const render = (ast) => {
+  const gendiff = ast.map((el) => {
+    const {
+      type, key, value, meta,
+    } = el;
 
-  const render = (ast) => {
-    const gendiff = ast.map((el) => {
-      const {
-        type, key, value, meta,
-      } = el;
+    if (el.type === 'updated') {
+      return `  - ${key}: ${meta.oldValue}\n  + ${key}: ${value}`;
+    }
+    return `  ${symbols[type]} ${key}: ${value}`;
+  });
+  return `{\n${gendiff.join('\n')}\n}`;
+};
 
-      if (el.type === 'updated') {
-        return `  - ${key}: ${meta.oldValue}\n  + ${key}: ${value}`;
-      }
-      return `  ${symbols[type]} ${key}: ${value}`;
-    });
-    return `{\n${gendiff.join('\n')}\n}`;
-  };
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+export const getFullPath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = dirname(__filename);
-  export const getFullPath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-
-
-  export const gendiff = (filepath1, filepath2) => {
-    const paths = [filepath1, filepath2];
-    const data = paths.map((filepath) => {
-      const fullPaht = getFullPath(filepath);
-      const content = fs.readFileSync(fullPaht, 'utf-8');
-      return JSON.parse(content);
-    });
-    const tree = buildAST(data);
-    return render(tree);
-  };
+export const gendiff = (filepath1, filepath2) => {
+  const paths = [filepath1, filepath2];
+  const data = paths.map((filepath) => {
+    const fullPaht = getFullPath(filepath);
+    const content = fs.readFileSync(fullPaht, 'utf-8');
+    return JSON.parse(content);
+  });
+  const tree = buildAST(data);
+  return render(tree);
+};
