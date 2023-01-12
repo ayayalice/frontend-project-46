cad15adcc93d194597b8af3ec18fecf812d6c802
Alice <aliceicelie@gmail.com>
2023-01-12 12:13:03 +0300

    add tests

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -5,5 +5,5 @@ import { getFullPath, gendiff } from '../src/index.js';
 const result = fs.readFileSync(getFullPath('result.txt'), 'utf8');
 
 test('genDiff', () => {
-  expect(gendiff('file1.json', 'file2.json')).toBe(result);
+  expect(gendiff('file1.json', 'file2.json')).toEqual(result);
 });
--- makefile
+++ makefile
@@ -1,6 +1,8 @@
 install:
 	npm ci
 lint:
-	npx eslint .
+	npx eslint --fix .
 tests:
 	NODE_OPTIONS=--experimental-vm-modules npx jest
+tests-coverage:
+	NODE_OPTIONS=--experimental-vm-modules npx jest --coverage
