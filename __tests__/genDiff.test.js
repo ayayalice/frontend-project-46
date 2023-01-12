7f464967cc623c6dcec605017673afe705dd28b5
Alice <aliceicelie@gmail.com>
2023-01-12 12:52:20 +0300

    fix tests

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -4,6 +4,13 @@ import { getFullPath, gendiff } from '../src/index.js';
 
 const result = fs.readFileSync(getFullPath('result.txt'), 'utf8');
 
-test('genDiff', () => {
-  expect(gendiff('file1.json', 'file2.json')).toEqual(result);
+test('compare two files shoold return difference', () => {
+  expect(gendiff('file1.json', 'file2.json')).toMatch('{');
+  expect(gendiff('file1.json', 'file2.json')).toMatch('- follow: false');
+  expect(gendiff('file1.json', 'file2.json')).toMatch('host: hexlet.io');
+  expect(gendiff('file1.json', 'file2.json')).toMatch('- proxy: 123.234.53.22');
+  expect(gendiff('file1.json', 'file2.json')).toMatch('- timeout: 50');
+  expect(gendiff('file1.json', 'file2.json')).toMatch('+ timeout: 20');
+  expect(gendiff('file1.json', 'file2.json')).toMatch('+ verbose: true');
+  expect(gendiff('file1.json', 'file2.json')).toMatch('}');
 });
--- src/index.js
+++ src/index.js
@@ -12,12 +12,12 @@ const mknode = (key, value, type, meta = {}) => ({
 
 const buildAST = (objects) => {
   const [obj1, obj2] = objects;
-  const key1 = _.keys(obj1);
-  const key2 = _.keys(obj2);
-  const keys = _.union(key1, key2);
+  const keys = _.union(_.keys(obj1), _.keys(obj2));
   const sortedKeys = _.sortBy(keys);
   const nodes = sortedKeys.map((key) => {
-    const [value1, value2] = [obj1[key], obj2[key]];
+    const key1 = obj1[key];
+    const key2 = obj2[key];
+    const [value1, value2] = [key1, key2];
     if (!_.has(obj1, key)) return mknode(key, value2, 'added');
     if (!_.has(obj2, key)) return mknode(key, value1, 'removed');
     if (value1 !== value2) return mknode(key, value2, 'updated', { oldValue: value1 });
