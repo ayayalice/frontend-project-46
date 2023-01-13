0b8de514fa69f8cc5c827620fd95395649904f76
Alice <aliceicelie@gmail.com>
2023-01-13 12:20:30 +0300

    fix tests

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -3,7 +3,7 @@ import fs from 'fs';
 import { getFullPath, gendiff } from '../src/index.js';
 
 const result = fs.readFileSync(getFullPath('result.txt'), 'utf8');
-
+console.log(result);
 test('compare two files shoold return difference', () => {
   expect(gendiff('file1.json', 'file2.json')).toEqual(result);
 });
