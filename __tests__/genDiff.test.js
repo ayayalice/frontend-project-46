73ceaa6e781d7c86ebad73913ffd61c51316c823
Alice <aliceicelie@gmail.com>
2023-01-16 18:05:31 +0300

    fix codeclimate

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -6,18 +6,8 @@ const result = fs.readFileSync(getFullPath('result.txt'), 'utf8');
 const plainResult = fs.readFileSync(getFullPath('plainResult.txt'), 'utf8');
 const jsonResult = fs.readFileSync(getFullPath('jsonResult.txt'), 'utf8');
 
-test('compare two files should return difference', () => {
-  expect(gendiff('file1.json', 'file2.json')).toEqual(result);
-  expect(gendiff('file1.yml', 'file2.yml')).toEqual(result);
-  expect(gendiff('file1.yml', 'file2.json')).toEqual(result);
-});
-test('compare two files should show properties', () => {
-  expect(gendiff('file1.json', 'file2.json', 'plain')).toEqual(plainResult);
-  expect(gendiff('file1.yml', 'file2.yml', 'plain')).toEqual(plainResult);
-  expect(gendiff('file1.yml', 'file2.json', 'plain')).toEqual(plainResult);
-});
-test('compare two files should show properties', () => {
-  expect(gendiff('file1.json', 'file2.json', 'json')).toEqual(jsonResult);
-  expect(gendiff('file1.yml', 'file2.yml', 'json')).toEqual(jsonResult);
-  expect(gendiff('file1.yml', 'file2.json', 'json')).toEqual(jsonResult);
+test('genDiff', () => {
+  expect(gendiff('file1.json', 'file2.yml')).toEqual(result);
+  expect(gendiff('file1.json', 'file2.yml', 'plain')).toEqual(plainResult);
+  expect(gendiff('file1.json', 'file2.yml', 'json')).toEqual(jsonResult);
 });
