43056c8c44eea324a282b21d2d96c56ba3fba119
Alice <aliceicelie@gmail.com>
2023-01-17 17:50:02 +0300

    fix error

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import formatData from './formatters/index.js';
 const buildAbsolutePath = (filepath) => path.resolve(process.cwd(), filepath);
 const getFormat = (filepath) => path.extname(filepath).slice(1);
 const getData = (filepath) => parse(fs.readFileSync(filepath, 'utf-8'), getFormat(filepath));
-
 const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
   const data1 = getData(buildAbsolutePath(filepath1));
   const data2 = getData(buildAbsolutePath(filepath2));
