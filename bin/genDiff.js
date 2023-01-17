ce4234ae6ed469f045ee8a4581f6ff6866382941
Alice <aliceicelie@gmail.com>
2023-01-17 18:49:00 +0300

    fix error

--- bin/genDiff.js
+++ bin/genDiff.js
@@ -5,7 +5,7 @@ import genDiff from '../src/index.js';
 const program = new Command();
 
 program
-  .name('gendiff')
+  .name('genDiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0', '-V, --version', 'output the version number')
   .option('-f, --format <type>', 'output format')
