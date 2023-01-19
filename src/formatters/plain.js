3a0633c288f0a8f7d4f88a1c20a773118db888c9
Alice <aliceicelie@gmail.com>
2023-01-19 20:44:58 +0300

    fix error

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -2,6 +2,7 @@ import _ from 'lodash';
 
 const stringify = (data) => {
   if (_.isObject(data)) return '[complex value]';
+  if (data === null) return 'null';
   if (typeof data === 'string') return `'${data}'`;
 
   return String(data);
